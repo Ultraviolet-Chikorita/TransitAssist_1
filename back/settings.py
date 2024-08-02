03e044677d3ac920d73902df98fd9c8b7b0d693a
Ultraviolet-Chikorita <147429542+Ultraviolet-Chikorita@users.noreply.github.com>
2024-08-02 15:52:24 +0100

    Update settings.py

--- back/settings.py
+++ back/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'social_django.middleware.SocialAuthExceptionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'back.urls'
