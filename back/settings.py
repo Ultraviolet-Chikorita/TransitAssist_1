5b83f7bc24d1476c8da0dbc48f870b3ff7f7e7b3
Ultraviolet-Chikorita <147429542+Ultraviolet-Chikorita@users.noreply.github.com>
2024-08-02 15:46:00 +0100

    Update settings.py

--- back/settings.py
+++ back/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&j*-33wt4zc11mv6(8xxllvmqqwc0qdeu*w47ata*1$rmadg2t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "transitassist-1.onrender.com"]
 
